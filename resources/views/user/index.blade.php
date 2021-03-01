ec7c2e87b0681a0befa8cdd949d67a452dbad189
xcrone <zahircrone@gmail.com>
2021-03-01 20:21:41 +0800

    Update index.blade.php

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -8,9 +8,22 @@
                 <div class="card-header">User</div>
 
                 <div class="card-body">
-                    @foreach ($users as $user)
-                        <div class="col-12">{{ $user->name }}</div>
-                    @endforeach
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr>
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $user->email }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
